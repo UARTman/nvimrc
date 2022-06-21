ebd586f776ffd43f68f88a214a0f6d4d9cc7739e
Anton Gusev <uartman@mail.rui>
2022-06-21 15:42:01 +0300

    add basiac lsp keybindings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -4,7 +4,43 @@ require("nvim-lsp-installer").setup {
 
 lspconfig = require('lspconfig')
 
-lspconfig.ccls.setup {}
-lspconfig.rust_analyzer.setup {}
-lspconfig.sumneko_lua.setup {}
-lspconfig.asm_lsp.setup {}
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+-- local opts = { noremap=true, silent=true }
+-- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+-- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+local on_attach = function(client, bufnr)
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<Leader>lwa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<Leader>lwr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<Leader>lwl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<Leader>lD', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<Leader>lrn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<Leader>lca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<Leader>lf', vim.lsp.buf.formatting, bufopts)
+end
+
+lspconfig.ccls.setup {
+    on_attach = on_attach,
+}
+lspconfig.rust_analyzer.setup {
+    on_attach = on_attach,
+}
+lspconfig.sumneko_lua.setup {
+    on_attach = on_attach,
+}
+lspconfig.asm_lsp.setup {
+    on_attach = on_attach,
+}
